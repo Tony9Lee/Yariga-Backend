2ab61ed6ff2b57d87aa7f37bbe4d58866b417527
tony l <33114223+Tony9Lee@users.noreply.github.com>
2023-03-12 19:49:59 -0700

    Day4

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -1,6 +1,7 @@
-import Property from "../mongodb/models/user.js";
+import Property from "../mongodb/models/property.js";
 import User from "../mongodb/models/user.js";
 
+import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
 import { v2 as cloudinary } from 'cloudinary';
 
@@ -12,43 +13,57 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
-const getAllProperties = async (req, res) => {};
-const getPropertyDetail = async (req, res) => {};
-const createProperty = async (req, res) => {
-
+const getAllProperties = async (req, res) => {
+  const query = {};
+  
   try {
-    
-    const { title, description, propertyType, location, price, photo, email } = req.body;
-  
-    //Start a new session...
-    const session = await mongoose.startSession();
-    session.startTransaction();
-  
-    const user = await User.findOne({ email }).session(session);
-  
-    if(!user) throw new Error('User not found');
-  
-    const photoUrl = await cloudinary.uploader.upload(photo);
-  
-    const newProperty = await Property.create({
-      title,
-      description,
-      propertyType,
-      location,
-      price,
-      photo: photoUrl.url,
-      creator: user._id
-    });
-  
-    user.allProperties.push(newProperty._id);
-    await user.save({ session });
-  
-    await session.commitTransaction();
-  
-    res.status(200).json({message: 'Property created successfully' });
+    const properties = await Property.find({}).limit(req,query._end);
+
+    res.status(200).json(properties);
   } catch (error) {
     res.status(500).json({ message: error.message })
-    
+  }
+};
+const getPropertyDetail = async (req, res) => {};
+const createProperty = async (req, res) => {
+  try {
+      const {
+          title,
+          description,
+          propertyType,
+          location,
+          price,
+          photo,
+          email,
+      } = req.body;
+
+      const session = await mongoose.startSession();
+      session.startTransaction();
+
+      const user = await User.findOne({ email }).session(session);
+
+      if (!user) throw new Error("User not found");
+
+      const photoUrl = await cloudinary.uploader.upload(photo);
+
+      const newProperty = await Property.create({
+          title,
+          description,
+          propertyType,
+          location,
+          price,
+          photo: photoUrl.url,
+          creator: user._id,
+      });
+
+      user.allProperties.push(newProperty._id);
+      await user.save({ session });
+
+      await session.commitTransaction();
+
+      res.status(200).json({ message: "Property created successfully" });
+  } catch (error) {
+      res.status(500).json({ message: error.message });
   }
 };
 
